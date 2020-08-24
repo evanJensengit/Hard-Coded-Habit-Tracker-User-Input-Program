b7b7e3b2332dafc1974c5ea8b85d6984de3019c4
Evan Jensen <53125480+evanJensengit@users.noreply.github.com>
2020-08-23 20:24:50 -0700

    hardcoded ForEvanAccountability
    added the wrkt corrections and therapy. Needs to print to file BEFORE the last entry not after

--- main.cpp
+++ main.cpp
@@ -9,20 +9,10 @@
 #include <fstream>
 using namespace std;
 
-//have classes for the date and a way to create classes of previous entries to figure out where
-//I stand during the week with the list of tasks to complete like wanting to do 3 scj entries
-//3 IFS sessions and 4 cbt sessions, wrkt 3 times a week, sleep 8 hrs a day, meditate 20 mins a day
-// and certain amount weekly to add up to a week. I have to have an automatic entry so that if
-//I miss a day it says "no entry" but still shows the date on the text editor with "no entry"
-//then at the end of the week it pulls all the data from the previous weeks worth of entries and I can
-//see what the stats are.
-//^^ in the program have a function that pulls the last weeks entries, compiles it and shows
-//stats of how much I did/have done. ANd if it is the second day of that week then it knows only to show
-//today and the day before stats and no stats from the previous week. Also have a method to show a previous
-//week's stats
 int main(int argc, const char * argv[]) {
    string date, time, weekday, feeling, intensity,
-   wrkt;
+   wrkt, bible, didRead, code, didCode, sleepAmount,
+   didTherapy, therapy;
    
    cout << "What is the date?" << endl;
    getline(cin, date); // use this to get the whole line
@@ -32,26 +22,69 @@ int main(int argc, const char * argv[]) {
    
    cout << "What is the time?" << endl; //how to pull the time from the internet??
    getline(cin, time);
-   cout << "How are you feeling?" << endl;
-   getline(cin, feeling);
-
-   cout << "On a scale of 1 - 10 (1 being very low"
-   " intensity 10 being super intense)" << endl;
-   getline(cin, intensity);
    
    cout << "Did you workout today? Y/N" << endl;
    getline(cin, wrkt);
    
    if (wrkt == "Y") {
-      
+      cout << "What kind of work out? " << endl;
+      getline(cin, wrkt);
+   }
+   else {
+      wrkt = "Did not work out";
+   }
+   
+   cout << "How much sleep did you get last night? (just enter number) " << endl;
+   getline(cin, sleepAmount);
+   
+   cout << "Did you read a face of the bible? Y/N" << endl;
+   getline(cin, bible);
+   
+   if (bible == "Y") {
+      cout << "What chapter(s) did you read and what stood out to you? " << endl;
+      getline(cin, bible);
+      didRead = "Yes ";
+   }
+   else {
+      bible = "Did not read bible";
+      didRead = "No ";
+   }
+   
+   cout << "Did you do any csci studying? Y/N" << endl;
+   getline(cin, didCode);
+   
+   if (didCode == "Y") {
+      cout << "What csci did you do specifically?" << endl;
+      getline(cin, code);
+      didCode = "Yes ";
+   }
+   else {
+      code = "Did not study csci";
+      didCode = "No ";
+   }
+   
+   cout << "Did you do any therapy work? Y/N" << endl;
+   getline(cin, didTherapy);
+   if (didTherapy == "Y") {
+      cout << "What therapy did you do?" << endl;
+      getline(cin, therapy);
+      didTherapy = "Yes ";
+   }
+   else {
+      therapy = "Did not do therapy work ";
+      didTherapy = "No ";
    }
    ofstream myfile;
    myfile.open ("accountability.txt", std::ios_base::app);
    myfile << "Date: " << date << endl;
-   myfile << "Time: " << time << endl;
    myfile << "Weekday: " << weekday << endl;
-   myfile << "Feeling: " << feeling << endl;
-   myfile << "Intensity of: " << feeling << " was " << intensity << endl;
+   myfile << "Time: " << time << endl;
+   myfile << "Workout: " << wrkt << endl;
+   myfile << "Sleep last night: " << sleepAmount << endl;
+   myfile << "Read Bible: " << didRead << bible << endl;
+   myfile << "Study csci: " << didCode << code << endl;
+   myfile << "Therapy work: " << didTherapy << therapy << endl << endl;
+   
    myfile.close();
    return 0;
 }
